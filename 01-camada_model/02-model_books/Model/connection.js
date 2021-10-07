ce9cc17e10db6ca88ba7d1f1b90b8fbdf43824e2
diego-vini <analistamm@gmail.com>
2021-10-07 15:51:19 -0300

    Criando nova camada model

--- 01-camada_model/02-model_books/Model/connection.js
+++ 01-camada_model/02-model_books/Model/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   user: 'root',
-  password: 'Dg@813650',
+  password: 'senha',
   port: 3307,
   host: 'localhost',
   database: 'model_example' 

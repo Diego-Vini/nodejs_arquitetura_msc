49a6a88a3f063a4b39a18d2894fe81215211b163
diego-vini <analistamm@gmail.com>
2021-10-06 21:10:11 -0300

    Criando a camada model e conectando ao banco mysql

---
+++ 01-camada_model/Model/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  user: 'root',
+  password: 'senha',
+  port: 3307,
+  host: 'localhost',
+  database: 'model_example'
+});
+
+module.exports = connection;

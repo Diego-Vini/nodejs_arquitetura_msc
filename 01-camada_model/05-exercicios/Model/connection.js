cbade5a431c11bc89e4868e4b6dd500bd72d0514
diego-vini <analistamm@gmail.com>
2021-10-10 19:40:48 -0300

    Iniciando arquivo model

---
+++ 01-camada_model/05-exercicios/Model/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  
+  const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+const connection = () => {
+    return MongoClient
+      .connect(MONGO_DB_URL, OPTIONS)
+      .then((conn) => conn.db(''))
+      .catch((err) => {
+        console.error(err);
+        process.exit()
+      });
+}
